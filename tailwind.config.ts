e24014d88144e6e1adde9cdd1e7f3aa9009288c9
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-06-30 17:35:11 -0300

    chore: add assets and config setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { DEFAULT_CIPHERS } from "tls";
 
 const config: Config = {
   content: [
@@ -7,11 +8,32 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1400px",
+    },
+    fontFamily: {
+      oswald: "var(--font-oswald)",
+      roboto: "var(--font-roboto)",
+    },
+    backgroundImage: {
+      hero: "url(/assets/img/hero/bg.png)",
+      membership: "url(/assets/img/menmbership/bg.png)",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          DEFAULT: "#333",
+          100: "#484848",
+          200: "#151515",
+          300: "#111",
+        },
       },
     },
   },
