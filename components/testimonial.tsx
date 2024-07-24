573786d035700c861be6ef888d33709e4116bb77
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-07-24 13:59:11 -0300

    feat: section testimonial done

--- components/testimonial.tsx
+++ components/testimonial.tsx
@@ -1,6 +1,105 @@
+"use client";
+
+import { FaQuoteLeft } from "react-icons/fa6";
+import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "@/lib/variants";
+
+const testimonialData = [
+  {
+    img: "/assets/img/testimonial/lucy.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iusto iure nam architecto quis ratione.",
+    name: "Lucy Antonny",
+  },
+
+  {
+    img: "/assets/img/testimonial/maria.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iusto iure nam architecto quis ratione.",
+    name: "Maria Rose",
+  },
+
+  {
+    img: "/assets/img/testimonial/michael.jpg",
+    message:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iusto iure nam architecto quis ratione.",
+    name: "Michael Smith",
+  },
+];
+
 const Testimonial = () => {
   return (
-    <section className="h-[50vh] bg-indigo-300" id="testimonial"></section>
+    <section className="py-12 xl:py-28" id="testimonial">
+      <div className="container mx-auto">
+        <motion.h2
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="h2 text-center"
+        >
+          Our Testimonials
+        </motion.h2>
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            modules={[Pagination]}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              },
+            }}
+            className="h-[320px]"
+          >
+            {testimonialData.map((person, index) => {
+              return (
+                <SwiperSlide className="w-full" key={index}>
+                  <div className="flex flex-col justify-center items-center gap-6 text-center h-full">
+                    <Image
+                      className="rounded-full border-2 border-red-500 "
+                      src={person.img}
+                      width={90}
+                      height={90}
+                      alt={person.name}
+                    />
+                    <div className="flex flex-col justify-center items-center">
+                      <FaQuoteLeft className="text-2xl text-gray-300" />
+                      <p className="max-w-[300px] mb-4">{person.message}</p>
+                      <span className="text-2xl text-red-500">
+                        {person.name}
+                      </span>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </motion.div>
+      </div>
+    </section>
   );
 };
 
